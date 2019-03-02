2c38586bed93b5e9e8d53243497c52c99bb46a71
Viacheslav Klepach <ualinkedin@gmail.com>
2019-03-02 12:34:16 +0200

    Added comments for functions

--- todo-app.js
+++ todo-app.js
@@ -1,5 +1,3 @@
-console.log('Yo')
-
 const todos = [{
     text: 'Order cat food',
     completed: true
@@ -24,22 +22,20 @@ const todos = [{
 //     p.remove()
 // });
 
-//Add an element
-// const newParagraph = document.createElement('p')
-// newParagraph.textContent = 'This is a new element from Javascript'
-// document.querySelector('body').appendChild(newParagraph)
-
+//initial filter pattern
 const filters = {
     searchText: ''
 }
 
+//filters and renders todos based on the filter pattern
 const renderTodos = function (todos, filters) {
+    //filters todos based on the filter pattern
     const filteredTodos = todos.filter(function (todo){
         return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
     })
-
+    //clears initial todos
     document.querySelector('#todos').innerHTML = ''
-
+    //renders filterred todos
     filteredTodos.forEach(function (todo) {
         if (!todo.completed) {
         const newParagraph = document.createElement('p')
@@ -49,12 +45,13 @@ const renderTodos = function (todos, filters) {
     });
 }
 
+//renders initial todos
 renderTodos(todos, filters)
 
 document.querySelector('button#add-todo').addEventListener('click', function(e){
     e.target.textContent = 'Added!'
 })
-
+//updates filter pattern based on input
 document.querySelector('input#new-todo-text').addEventListener('input', function(e){
     filters.searchText = e.target.value
     renderTodos(todos, filters)
