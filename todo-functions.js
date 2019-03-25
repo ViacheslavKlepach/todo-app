b0fb64e6dbb1e6ce083b462b2e712e9aa9dab4f1
ViacheslavKlepach <ualinkedin@gmail.com>
2019-03-25 09:36:59 +0300

    Added toggleCheckbox

--- todo-functions.js
+++ todo-functions.js
@@ -15,6 +15,26 @@ const SaveTodos = function (todos) {
     localStorage.setItem('todos', JSON.stringify(todos))
 }
 
+//Remove Todo from the list
+const removeTodo = function (id) {
+    const nodeIndex = todos.findIndex(function (todo) {
+        return todo.id === id
+    })
+    if (nodeIndex > -1) {
+        todos.splice(nodeIndex, 1)
+    }
+}
+
+//Toggle the completed value for a given  todo
+const toggleTodo = function (id) {
+    const todo = todos.find(function (todo) {
+        return todo.id === id
+    })
+    if (todo !== undefined) {
+        todo.completed = !todo.completed
+    }
+}
+
 //Render application todos based on filters
 const renderTodos = function (todos, filters) {
     //filters todos based on the filter pattern and by value from hide-completed checkbox
@@ -41,7 +61,13 @@ const generateTodoDOM = function (todo) {
 
     //Setup todo checkbox
     checkTodo.setAttribute('type', 'checkbox')
+    checkTodo.checked = todo.completed
     todoEl.appendChild(checkTodo)
+    checkTodo.addEventListener('change', function () {
+        toggleTodo(todo.id)
+        SaveTodos(todos)
+        renderTodos(todos, filters)
+    })
 
     //Setup the todo text
     textEl.textContent = todo.text
@@ -49,7 +75,12 @@ const generateTodoDOM = function (todo) {
 
     //Setup the remove button
     removeButton.textContent = 'x'
-    todoEl.appendChild(removeButton)    
+    todoEl.appendChild(removeButton)
+    removeButton.addEventListener('click', function () {
+        removeTodo(todo.id)
+        SaveTodos(todos)
+        renderTodos(todos, filters)
+    })
     
     return todoEl
 }
